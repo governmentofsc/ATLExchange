9b70a21ff0463c13f7ac4b0e14f9f9b6d815bf13
governmentofsc <governmentofsc@gmail.com>
2025-10-20 22:09:42 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: "project-5b536.firebaseapp.com",
   databaseURL: "https://project-5b536-default-rtdb.firebaseio.com",
   projectId: "project-5b536",
-  storageBucket: "project-5b536.firebasestorage.app",
+  storageBucket: "project-5b536.fiebasestorage.app",
   messagingSenderId: "18490330325",
   appId: "1:18490330325:web:dd336aab1cc2899783a65c",
   measurementId: "G-L1NZSG10D0"
